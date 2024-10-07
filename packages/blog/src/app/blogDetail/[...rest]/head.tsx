29e7b9623b4b230cbc55658b9431a6a996d69198
Notexistboy <hansong.pedro@bytedance.com>
2024-10-07 20:35:46 +0800

    feat: fix eslint

--- packages/blog/src/app/blogDetail/[...rest]/head.tsx
+++ packages/blog/src/app/blogDetail/[...rest]/head.tsx
@@ -1,12 +1,61 @@
 import dayjs from 'dayjs';
 import Link from 'next/link';
 
-import data from '@blog/communication';
+// import data from '@blog/communication';
 import { FC, Suspense } from 'react';
 import { CopyIcon } from './client';
 
 interface Props {
-  current: (typeof data.issuesData)[number];
+  current: IssuerData;
+}
+
+interface IssuerData {
+  url: string;
+  repository_url: string;
+  labels_url: string;
+  comments_url: string;
+  events_url: string;
+  html_url: string;
+  id: number;
+  node_id: string;
+  number: number;
+  title: string;
+  labels: {
+    id: number;
+    node_id: string;
+    url: string;
+    name: string;
+    color: string;
+    default: false;
+    description: string;
+  }[];
+  state: string;
+  locked: false;
+  assignee: null;
+  assignees: [];
+  milestone: null;
+  comments: number;
+  created_at: string;
+  updated_at: string;
+  closed_at: null;
+  author_association: string;
+  active_lock_reason: null;
+  body: string;
+  reactions: {
+    url: 'https://api.github.com/repos/bosens-China/blog/issues/101/reactions';
+    total_count: 0;
+    '+1': 0;
+    '-1': 0;
+    laugh: 0;
+    hooray: 0;
+    confused: 0;
+    heart: 0;
+    rocket: 0;
+    eyes: 0;
+  };
+  timeline_url: string;
+  performed_via_github_app: null;
+  state_reason: null;
 }
 
 export const Head: FC<Props> = ({ current }) => {
